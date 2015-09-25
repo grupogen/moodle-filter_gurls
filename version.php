677f6bfb863116f3a6658765835631d54d925309
Northern Links <fernandooliveira@knet.ca>
2015-09-25 14:12:29 -0400

    Update version.php

--- version.php
+++ version.php
@@ -16,7 +16,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2015082501;        // The current plugin version (Date: YYYYMMDDXX)
+$plugin->version   = 2015092501;        // The current plugin version (Date: YYYYMMDDXX)
 $plugin->requires  = 2013110500;        // Requires this Moodle version
 $plugin->component = 'filter_gurls';  // Full name of the plugin (used for diagnostics)
 $plugin->release = 'v2.7-r1'; // first revision
